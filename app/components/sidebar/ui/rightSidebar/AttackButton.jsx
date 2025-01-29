7c52aa6165ca8f7748caf4e36d7fb83ea49fac6c
Hanbee Lee <bee1162@naver.com>
2025-01-29 18:04:39 +0900

    feat: hover해서 보더 추가

--- app/components/sidebar/ui/rightSidebar/AttackButton.jsx
+++ app/components/sidebar/ui/rightSidebar/AttackButton.jsx
@@ -1,6 +1,6 @@
 const AttackButton = ({ name, onClick }) => {
     return (
-      <button className="w-full p-4 bg-[#2A2A2A] hover:bg-[#C5C5C5] rounded-lg text-left transition-colors">
+      <button className="w-full p-4 bg-[#2A2A2A] hover:bg-[#292E30] rounded-lg text-left transition-colors hover:border-[#C3C3C3] hover:border-2">
         {name}
       </button>
     );
