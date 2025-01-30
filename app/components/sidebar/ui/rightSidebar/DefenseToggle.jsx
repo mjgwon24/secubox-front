ba04aab8a5bf8e2e9fbd1fdb4b4c9281eb63b7b4
Hanbee Lee <bee1162@naver.com>
2025-01-30 23:29:59 +0900

    feat: 토글 스위치 추가

--- app/components/sidebar/ui/rightSidebar/DefenseToggle.jsx
+++ app/components/sidebar/ui/rightSidebar/DefenseToggle.jsx
@@ -1,12 +1,12 @@
+import { Switch, ToggleButton } from "@mui/material";
 import { useState } from "react";
 const DefenseToggle = ({ name, enabled, onToggle }) => {
-    return (
-      <div className="flex items-center justify-between w-full p-4 bg-[#2A2A2A] rounded-lg">
-        <span>{name}</span>
-        
-      </div>
-    );
-  };
-  
-  export default DefenseToggle;
-  
+  return (
+    <div className="flex items-center justify-between w-full p-4 bg-[#2A2A2A] rounded-lg">
+      <span>{name}</span>
+      <Switch color="default" />
+    </div>
+  );
+};
+
+export default DefenseToggle;
