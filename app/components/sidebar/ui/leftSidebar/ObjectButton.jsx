b9b547263430fa79b900aba997912399d54c3f09
mjgwon24 <alswlchlrh8@naver.com>
2025-02-08 18:03:06 +0900

    feat: 드래그앤 드롭 세부 기능 개선 (2차)

--- app/components/sidebar/ui/leftSidebar/ObjectButton.jsx
+++ app/components/sidebar/ui/leftSidebar/ObjectButton.jsx
@@ -1,27 +1,28 @@
 "use client";
 
-import {useDrag} from "react-dnd";
+import { useDrag } from "react-dnd";
+import { v4 as uuidv4} from "uuid";
 
-export const ObjectButton = ({ iconKey, name, onclick}) => {
-    const [{isDragging}, drag] = useDrag(() => ({
+export const ObjectButton = ({ iconKey, name, onDragStart }) => {
+    const [{ isDragging }, drag] = useDrag(() => ({
         type: "OBJECT",
-        item: {id: iconKey, name},
+        item: onDragStart ? onDragStart() : { id: uuidv4(), name },
         collect: (monitor) => ({
-            isDragging: monitor.isDragging()
-        })
+            isDragging: monitor.isDragging(),
+        }),
     }));
 
     return (
-       <button
-           ref={drag}
-           className={`w-[90px] h-[103px] bg-[#2A2A2A] rounded-lg transition-colors outline-none hover:outline hover:outline-[#C3C3C3] hover:outline-2 hover:bg-[#292E30] 
-                     ${isDragging ? "opacity-50" : ""}`}
-           onClick={onclick}>
-           <div className="flex flex-col items-center justify-between h-full py-6">
-                <img src={`/icons/objects/${iconKey}.png`} alt={name}
-                     className="h-7"/>
+        <button
+            ref={drag}
+            className={`w-[90px] h-[103px] bg-[#2A2A2A] rounded-lg transition-colors outline-none 
+                hover:outline hover:outline-[#C3C3C3] hover:outline-2 hover:bg-[#292E30] 
+                ${isDragging ? "opacity-50" : ""}`}
+        >
+            <div className="flex flex-col items-center justify-between h-full py-6">
+                <img src={`/icons/objects/${iconKey}.png`} alt={name} className="h-7" />
                 <p className="text-[#C5C5C5] text-[14px]">{name}</p>
-           </div>
-       </button>
+            </div>
+        </button>
     );
 };
